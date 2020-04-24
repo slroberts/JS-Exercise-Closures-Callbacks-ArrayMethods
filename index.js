2498b516df2c57224f3395953113c2fba1a05665
slroberts <shomariroberts@gmail.comclear>
2020-04-23 23:54:06 -0400

    Completed processProduct()

--- index.js
+++ index.js
@@ -112,8 +112,9 @@ function processSum(num1, num2, callback) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
  */
-function processProduct(/* CODE HERE */) {
+function processProduct(num1, num2, callback) {
   /* CODE HERE */
+  return callback(num1 * num2);
 }
 
 /**
